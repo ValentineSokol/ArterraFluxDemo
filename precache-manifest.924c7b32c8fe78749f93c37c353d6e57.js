ccb107494b9004fd4faea5d33e8c93c02725c075
ValentineSokol <53806015+ValentineSokol@users.noreply.github.com>
2020-04-22 21:52:48 +0300

    Updates

---
+++ precache-manifest.924c7b32c8fe78749f93c37c353d6e57.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5108e60609d303a31c03222498276aa5",
+    "url": "/ArterraFluxDemo/index.html"
+  },
+  {
+    "revision": "7aad7d88bdbf294da7cf",
+    "url": "/ArterraFluxDemo/static/css/2.ef8e8b31.chunk.css"
+  },
+  {
+    "revision": "7aad7d88bdbf294da7cf",
+    "url": "/ArterraFluxDemo/static/js/2.896430e1.chunk.js"
+  },
+  {
+    "revision": "41230239057e2aedde577ec1099f8653",
+    "url": "/ArterraFluxDemo/static/js/2.896430e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d170429562def6ef72b",
+    "url": "/ArterraFluxDemo/static/js/main.6f8d26ba.chunk.js"
+  },
+  {
+    "revision": "20dada75cb64cc585577",
+    "url": "/ArterraFluxDemo/static/js/runtime-main.92021a5b.js"
+  },
+  {
+    "revision": "636d6b5f7b228196bc270325befb638f",
+    "url": "/ArterraFluxDemo/static/media/error.636d6b5f.png"
+  },
+  {
+    "revision": "ef2536e4f71ab1254ed535e1b0f0fa1b",
+    "url": "/ArterraFluxDemo/static/media/logo.ef2536e4.png"
+  },
+  {
+    "revision": "ca1d9d6cd89b1f579aaa70d6fa2ca8dc",
+    "url": "/ArterraFluxDemo/static/media/success.ca1d9d6c.png"
+  }
+]);
